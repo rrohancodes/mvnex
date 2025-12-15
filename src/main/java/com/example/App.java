72d0d994a462750b8ee2bf7063a008eb9ed84195
Rohan <rohan150515454@gmail.com>
2025-12-15 20:09:14 +0530

    y

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,5 +2,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Welcome to the Example Application.");
     }
 }
